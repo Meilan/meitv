2fa8209109df698ded5c3efed509637471be25e6
Meilan Lin <meilan.lin@capgemini.com>
2014-02-10 17:01:54 +0100

    Embed Youtube video working

--- js/tv-premieres-app/controllers/mainController.js
+++ js/tv-premieres-app/controllers/mainController.js
@@ -1,6 +1,12 @@
 var meilanTVControllers = angular.module('meilanTVControllers', []);
 
 
+// Function for generating valid url
+var generateUrl = function(pre, tail) {
+	var convertedUrl = pre.replace(/[\. ,:-]+/g, "-") + '-' + tail;
+	return convertedUrl;
+};
+
 meilanTVControllers.controller('ShopCtrl', ['$scope', '$routeParams',
   function($scope, $routeParams) {
 	
@@ -64,19 +70,19 @@ meilanTVControllers.controller('mainController', ['$scope', '$http',
 		$scope.orderField = "Year"; //Default order field
 		$scope.orderReverse = false;
 
+		$scope.embedTrailer = null;
+
+
 
 		//$scope.apiUrl = 'http://api.trakt.tv/movies/trending.json/' + $scope.apiKey + '?&callback=JSON_CALLBACK';
 		$scope.apiUrl = 'http://meitv.dev/data/data.json?callback=JSON_CALLBACK';
+		$scope.statsUrl = 'http://api.trakt.tv/movie/stats.json/'+ $scope.apiKey;
 
 		$scope.init = function() {
 			//user "jsonp" instead of get when it is cross domain fetching
 			$http.get($scope.apiUrl).success(function(data){
 				angular.forEach(data, function(value, index) {
-					//Conver normal youtube video url into embed url
-					var trailerUrl = value.trailer;
-					trailerUrl = trailerUrl.replace('watch?v=', 'embed/');
-					value.trailerUrl = trailerUrl;
-
+					
 					angular.forEach(value.genres, function(genre, index) {
 						var exists = false;
 						angular.forEach($scope.availableGenres, function(avGerne, index) {
@@ -115,79 +121,28 @@ meilanTVControllers.controller('mainController', ['$scope', '$http',
 				break;
 			}
 		};
-}]);
 
+		$scope.showStats = function(movie) {
+			var movieUrl = generateUrl(movie.title, movie.year);
+			var statsUrl = $scope.statsUrl + '/' + movieUrl + '?&callback=JSON_CALLBACK';
 
-/*
-meilanTVControllers.controller("mainController", function($scope, $http){
-
-	$scope.apiKey = "1c60bfaa49ab8a58b42043418a9cc049";
-	//$scope.searchQuery = "good";
-	$scope.filterText = null;
-	$scope.results = [];
-
-	$scope.itemsPerPage = 10;
-	$scope.currentPage = 0;
-
-	$scope.availableGenres = [];
-	$scope.genreFilter = null;
-
-	$scope.orderFields = [ "Year", "Rating"];
-	$scope.orderDirections = ["Descending", "Ascending"];
-	$scope.orderField = "Year"; //Default order field
-	$scope.orderReverse = false;
-
-
-	$scope.apiUrl = 'http://api.trakt.tv/movies/trending.json/' + $scope.apiKey + '?&callback=JSON_CALLBACK';
-
-	$scope.init = function() {
-		$http.jsonp($scope.apiUrl).success(function(data){
-			angular.forEach(data, function(value, index) {
-				
-				//Conver normal youtube video url into embed url
-				var trailerUrl = value.trailer;
-				trailerUrl = trailerUrl.replace('watch?v=', 'embed/');
-				value.trailerUrl = trailerUrl;
-
-				angular.forEach(value.genres, function(genre, index) {
-					var exists = false;
-					angular.forEach($scope.availableGenres, function(avGerne, index) {
-						if(avGerne == genre){
-							exists = true;
-						}
-					});
-					if(exists === false) {
-						$scope.availableGenres.push(genre);
-					}
-				});
-				$scope.results.push(value);
-
+			$http.jsonp(statsUrl).success(function(data){
+				//console.log(data);
 			});
-		}).error(function(error){ });
-	};
+		};
 
-	$scope.numberOfPages = function() {
-		console.log($scope.results.length);
-		return Math.ceil($scope.results.length/$scope.itemsPerPage);
-	};
+		$scope.showTrailer = function(movie) {
+			//Conver normal youtube video url into embed url
+			var trailerUrl = movie.trailer.replace('watch?v=', 'embed/');
+			$scope.embedTrailer = trailerUrl;
+			var newElement = angular.element("<iframe width='560' height='315' src="+$scope.embedTrailer+" frameborder='0' allowfullscreen></iframe><hr>");
+			$('#'+ movie.imdb_id).html(newElement);
 
-	$scope.setGenreFilter = function(genre) {
-		$scope.genreFilter = genre;
-	};
-
-	$scope.customOrder = function(movie) {
-		switch ($scope.orderField) {
-		case "Year":
-			return movie.year;
-			break;
-		case "Rating":
-			return movie.ratings.percentage;
-			break;
-		}
-	};
-
-});
-*/
+			$('#'+ movie.imdb_id).modal({
+				keyboard: true
+			});
+		};
+}]);
 
 //Pagination start point
 app.filter('startFrom', function() {
