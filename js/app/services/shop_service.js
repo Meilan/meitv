7369139be9101d7daada37b1b5e682ffcc14ec5a
Meilan Lin <meilan.lin@capgemini.com>
2014-02-13 16:44:45 +0100

    Clean up

--- js/app/services/shop_service.js
+++ js/app/services/shop_service.js
@@ -2,7 +2,7 @@
 // define(['app','angular-resource'], function (app) {
 define(['app'], function (app) {
     // app.register.factory('Pictures', ['$http', '$q', '$log', '$resource', function ($http, $q, $log, $resource) {
-    app.register.service('dvdItems', ['$http', function ($http) {
+    app.register.service('dvdItems', function () {
 
         var dvds = [
             {
@@ -26,6 +26,6 @@ define(['app'], function (app) {
 
         return dvds;
 
-    }]);
+    });
 
 });
